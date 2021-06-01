467f6904d9299d5ebf1f77c24dcf47208fee5550
shubhamkmt <kumawats@iitk.ac.in>
2021-06-01 11:02:21 +0530

    updated placement model

--- placement/models.py
+++ placement/models.py
@@ -1,17 +1,13 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
-
 
 class Placement(models.Model):
-    placement_text = models.CharField(max_length=200)
-    company = models.CharField(max_length=100)
+    placement_text = models.CharField(max_length=300)
+    company = models.CharField(max_length=300)
     role = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
-    eligible_batches = models.JSONField(default= dict )
-    eligible_branches = models.JSONField(default= dict )
-    eligible_programes = models.JSONField(default= dict )
+    description = models.CharField(max_length=500)
+    eligible_batches = models.JSONField(default= list )
+    eligible_branches = models.JSONField(default= list )
+    eligible_programes = models.JSONField(default= list )
     deadline = models.DateTimeField('deadline')
     def __str__(self):
         return self.placement_text
