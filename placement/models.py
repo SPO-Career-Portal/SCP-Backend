b1abe3d0564e4155caaf7a84e725dc2ca1eab667
shubhamkmt <kumawats@iitk.ac.in>
2021-06-04 22:01:03 +0530

    added new line and corrected the format

--- placement/models.py
+++ placement/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Placement(models.Model):
-    placement_text = models.CharField(max_length=300)
+    placement_name = models.CharField(max_length=300)
     company = models.CharField(max_length=300)
     role = models.CharField(max_length=100)
     description = models.CharField(max_length=1500)
-    eligible_batches = models.JSONField(default= list )
-    eligible_branches = models.JSONField(default= list )
-    eligible_programes = models.JSONField(default= list )
+    eligible_batches = models.JSONField(default=list)
+    eligible_branches = models.JSONField(default=list)
+    eligible_programes = models.JSONField(default=list)
     deadline = models.DateTimeField('deadline')
     def __str__(self):
         return self.placement_text
