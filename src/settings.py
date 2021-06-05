39d574b184ca8462d88052d41896f91bfbec42b1
Navneet Singh <78998610+navneet-28@users.noreply.github.com>
2021-06-06 01:25:41 +0530

    Add User Model (#10)
    * added user model
    
    * Update models.py
    
    Co-authored-by: Naman Gupta <32638824+namangup@users.noreply.github.com>

--- src/settings.py
+++ src/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "placement.apps.PlacementConfig",
     "intern.apps.InternConfig",
+    "user.apps.UserConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
