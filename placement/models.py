ebe553a3b54b070c73a604cedb7c9158b831282f
Shubham63-hub <61465339+Shubham63-hub@users.noreply.github.com>
2021-06-05 21:53:10 +0530

    Add Placement Model (#9)
    * Add Placement Model
    
    * Update models.py
    
    Co-authored-by: Naman Gupta <32638824+namangup@users.noreply.github.com>

---
+++ placement/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Placement(models.Model):
+    placement_name = models.CharField(max_length=300)
+    company = models.CharField(max_length=300)
+    role = models.CharField(max_length=100)
+    description = models.CharField(max_length=1500)
+    eligible_batches = models.JSONField(default=list)
+    eligible_branches = models.JSONField(default=list)
+    eligible_programmes = models.JSONField(default=list)
+    deadline = models.DateTimeField("deadline")
+
+    def __str__(self):
+        return self.placement_text
