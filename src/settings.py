c26a2403cf5040ca88a0df7a5f797d1a5aa4daf1
shubhamkmt <kumawats@iitk.ac.in>
2021-06-05 19:38:21 +0530

    Add Placement Model

--- src/settings.py
+++ src/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "placement.apps.PlacementConfig",
     "intern.apps.InternConfig",
     "django.contrib.admin",
     "django.contrib.auth",
