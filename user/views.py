726f73622a50b7d7ebdf47f2484476e2b1781514
udit <uditpd3000@gmail.com>
2021-06-09 17:07:59 +0530

    User API

--- user/views.py
+++ user/views.py
@@ -1,3 +1,24 @@
+from django.http import response
 from django.shortcuts import render
+from django import http
+from django.http.response import HttpResponse,JsonResponse
+from rest_framework import viewsets,permissions,status
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.serializers import Serializer
+from .serializers import userserializer
+from rest_framework.views import APIView
+from .models import User
+from rest_framework.response import Response
+from .utils import *
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+class Userview(APIView):
+   def get(self,request):
+       try:
+           user=Isloggedin(request)
+           if user is None :
+               return HttpResponse("Unauthorized",status=401)
+           return Response(userserializer.data,status=status.HTTP_200_OK) 
+        
+       except:
+            return HttpResponse("Bad Request",status=400)
