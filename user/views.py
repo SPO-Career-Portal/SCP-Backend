c0f0c42d352dd26e15e3d054ac57bb1b05ac6c37
parinayc20 <parinayc20@iitk.ac.in>
2021-06-11 18:44:36 +0530

    Changes included

--- user/views.py
+++ user/views.py
@@ -17,10 +17,8 @@ class UserPlacementsView(APIView):
                 eligible_placement_ids = list()
                 all_placements = Placement.objects.all()
                 for placement in all_placements:
-                    if user.program in placement.eligible_programmes:
-                        if user.department in placement.eligible_branches:
-                            if user.batch in placement.eligible_batches:
-                                eligible_placement_ids.append(placement.id)
+                    if user.program in placement.eligible_programmes and user.department in placement.eligible_branches and user.batch in placement.eligible_batches:
+                        eligible_placement_ids.append(placement.id)
                 eligible_placements = Placement.objects.filter(id__in=eligible_placement_ids)
                 serializer = PlacementSerializer(eligible_placements, many=True)
                 return Response(serializer.data, status=status.HTTP_200_OK)
@@ -38,10 +36,8 @@ class UserInternsView(APIView):
                 eligible_intern_ids = list()
                 all_interns = Intern.objects.all()
                 for intern in all_interns:
-                    if user.program in intern.eligible_programmes:
-                        if user.department in intern.eligible_branches:
-                            if user.batch in intern.eligible_batches:
-                                eligible_intern_ids.append(intern.id)
+                    if user.program in intern.eligible_programmes and user.department in intern.eligible_branches and user.batch in intern.eligible_batches:
+                        eligible_intern_ids.append(intern.id)
                 eligible_interns = Intern.objects.filter(id__in=eligible_intern_ids)
                 serializer = InternSerializer(eligible_interns, many=True)
                 return Response(serializer.data,status=status.HTTP_200_OK)
