396910a8d75337edf89d522950c9b4a896ef58f4
sanchit-sinha <sanchit4u2001@gmail.com>
2021-06-11 23:52:11 +0530

    Update Login-Logout API

--- src/urls.py
+++ src/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("auth/", include("user.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("user/", include("user.urls"))]
