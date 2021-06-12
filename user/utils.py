d5fcef2f04b919e9d8d2da28c8e79e6e17d12c0b
Naman Gupta <32638824+namangup@users.noreply.github.com>
2021-06-12 14:53:25 +0530

    Update utils.py

--- user/utils.py
+++ user/utils.py
@@ -2,8 +2,11 @@ from .models import User
 
 
 def IsLoggedIn(request):
-    try:
-        user = User.objects.get(username=request.session["username"])
-        return user
-    except:
+    if request.session.has_key("username"):
+        try:
+            user = User.objects.get(username=request.session["username"])
+            return user
+        except:
+            return None
+    else:
         return None
